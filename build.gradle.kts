34af53cbfe9b1e1ada61dce5aa3633185f8ba73b
Andre Ricardo <andrericardo.developer@outlook.com>
2024-10-13 19:01:43 -0300

    Configure Detekt - static code analyzer for Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.detekt.arturbosch.gitlab)
+}
+
+detekt {
+    toolVersion = libs.versions.detekt.get()
+    source.setFrom("app/src/main/java", "core/src/main/kotlin")
+    config.setFrom(file("$rootDir/config/detekt/detekt.yml"))
+    autoCorrect = true
 }
