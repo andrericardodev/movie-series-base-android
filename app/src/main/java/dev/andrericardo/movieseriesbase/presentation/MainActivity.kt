1ca7b3ee1ec2574c8cb413e66984d6260bc0a3e6
Andre Ricardo <andrericardo.developer@outlook.com>
2024-10-14 16:27:43 -0300

    Add BottomNavigation Menu

--- app/src/main/java/dev/andrericardo/movieseriesbase/presentation/MainActivity.kt
+++ app/src/main/java/dev/andrericardo/movieseriesbase/presentation/MainActivity.kt
@@ -3,8 +3,6 @@ package dev.andrericardo.movieseriesbase.presentation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -20,11 +18,6 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
 
