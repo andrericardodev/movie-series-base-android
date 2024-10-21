942f4ee3bccd8fb731d860978a8fa19d2d99c399
Andre Ricardo <andrericardo.developer@outlook.com>
2024-10-21 11:38:03 -0300

    Configure base navigation

--- app/src/main/java/dev/andrericardo/movieseriesbase/presentation/MainActivity.kt
+++ app/src/main/java/dev/andrericardo/movieseriesbase/presentation/MainActivity.kt
@@ -3,25 +3,54 @@ package dev.andrericardo.movieseriesbase.presentation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import dev.andrericardo.movieseriesbase.R
 import dev.andrericardo.movieseriesbase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-    private lateinit var appBarController: AppBarConfiguration
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
 
         navController = navHostFragment.navController
-        appBarController = AppBarConfiguration(setOf())
+        binding.bottomNavMain.setupWithNavController(navController)
+        appBarConfiguration =
+            AppBarConfiguration(
+                setOf(
+                    R.id.homeFragment,
+                    R.id.searchFragment,
+                    R.id.favoritesFragment,
+                    R.id.profileFragment,
+                ),
+            )
+
+        binding.topAppbarMain.setupWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val isTopLevelDestination =
+                appBarConfiguration.topLevelDestinations.contains(destination.id)
+            if (!isTopLevelDestination) {
+                binding.topAppbarMain.setNavigationIcon(R.drawable.ic_arrow_back_ios_new)
+            }
+        }
     }
 }
