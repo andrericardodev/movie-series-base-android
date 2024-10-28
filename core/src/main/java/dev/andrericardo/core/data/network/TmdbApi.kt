f54ee1e6003cdacf3e1bb288e4ccd7d897075a67
Andre Ricardo <andrericardo.developer@outlook.com>
2024-10-28 14:19:50 -0300

    Maps Popular Movies List endpoint

---
+++ core/src/main/java/dev/andrericardo/core/data/network/TmdbApi.kt
@@ -0,0 +1,13 @@
+package dev.andrericardo.core.data.network
+
+import dev.andrericardo.core.data.network.response.MovieListPopularResponse
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface TmdbApi {
+    @GET("/movie/popular")
+    suspend fun getMovieListPopular(
+        @QueryMap
+        queries: Map<String, String>,
+    ): MovieListPopularResponse
+}
